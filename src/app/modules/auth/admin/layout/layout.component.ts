cdab91397708c77bd8344f88b933a58e1156a865
mohammadreza.taherian <taherian.1993@gmail.com>
2025-12-04 21:47:25 +0400

    refactor : delete layout.component.css and refactor to tailwind

--- src/app/modules/auth/admin/layout/layout.component.ts
+++ src/app/modules/auth/admin/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button'; // Import
 import { AuthService } from '../../../../core/auth/services/auth.service';
 
@@ -8,7 +8,6 @@ import { AuthService } from '../../../../core/auth/services/auth.service';
   standalone: true,
   imports: [RouterOutlet, RouterLink, ButtonModule],
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
   private authService = inject(AuthService);
